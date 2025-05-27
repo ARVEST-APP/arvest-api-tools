4edb8fe5951d462e3645438eda44c2d5c5492060
Ivan.D <aryodeliae@gmail.com>
2025-05-27 16:14:27 +0200

    Markdown to manifest version propre

---
+++ Markdown to manifest/Manifest_1.md
@@ -0,0 +1,34 @@
+## Manifest title 
+|Label|Value|
+|------|-----|
+|Title|My first arvest manifest|
+|thumbnail|https://resource.arvest.app/efde0b16df31cc7dc9dbe65796ae77140aae0e18/thumbnail.webp|
+@@@t
+
+## Manifest metadata
+|Label|Value|
+|------|-----|
+|Title|My first arvest manifest|
+|Description|Its a good manifest|
+|Editor|Ivan Dubreucq|
+@@@m
+
+## Canavses
+|Source|Start|End|Titre|Format|
+|------|-----|---|---|---|
+|https://www.youtube.com/watch?v=DyJ_3O75Csc|00:00:00|00:29:22|Presentation|video|
+|https://images.unsplash.com/photo-1471899236350-e3016bf1e69e?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D|-|-|flower|image|
+@@@c
+
+## Link Annotations
+|Debut|Fin|Manifest|Contenu|
+|-----|---|----|-------|
+|00:00:00|00:00:00|https://resource.arvest.app/b603754e18300c2833ba734bca821143e60f9a22/Diapo_18_Delfina_S_Martinez_Pandian.json|Diapo|
+@@@la
+
+## Text Annotations
+|Canvas|Debut|Fin|Contenu|
+|------|-----|---|-------|
+|1|00:00:00|00:00:00|Diapo|
+|2|00:22:00|00:22:10|Your annotation|
+@@@ta
--- arvesttools/md_to_manifest/md_manifest.py
+++ arvesttools/md_to_manifest/md_manifest.py
@@ -10,51 +10,48 @@ from arvesttools.md_parser import recuperation, extraction_duration, extraction_
 def md_to_manifest(markdown_folder, manifest_folder, mail, password) :
 
     ar = arvestapi.Arvest(mail, password)
-    print(f"You are connecte as {ar.profile.name}")
 
-    num_fichier = 25
+    chemin = markdown_folder
 
-    nbr = os.listdir(markdown_folder)
-    md_count = 0
-    for i in nbr :
-        md_count += 1
+    #Extraction des informations pour les Canvas
+    Recuperation = recuperation(['## Canavses'],['@@@c'], chemin)
 
-    for i in range(1): 
+    link = extraction_colonne("Source", Recuperation)
+    format = extraction_colonne("Format", Recuperation)
 
-        chemin = os.path.join(markdown_folder, f"Manifest_{num_fichier}.md")
+    durations = extraction_duration2(Recuperation, 2, 3, format)
 
-        #Extraction des informations pour les Canvas
+    Base_info = recuperation(['## Manifest title '],['@@@t'], chemin)
+    titre = extraction_metadonne("Title", Base_info)
 
-        Recuperation = recuperation(['## Canavses'],['@@@c'], chemin)
-        link = extraction_colonne("Source", Recuperation)
-        format = extraction_colonne("Format", Recuperation)
-        durations = extraction_duration2(Recuperation, 2, 3, format)
-        Base_info = recuperation(['## Manifest title '],['@@@t'], chemin)
-        titre = extraction_metadonne("Title", Base_info)
+    num = 0
 
-        num = 0
+    #Géneration du manifeste
 
-        #Géneration du manifeste
+        
 
-        config.configs['helpers.auto_fields.AutoLang'].auto_lang = "en"
-        base_url = "http://127.0.0.1:5500"
-        manifest = Manifest(id=f"{base_url}/manifest_{num_fichier}.json", label=titre)
-
-        #Creation des Canvas 
-
-        Recuperation.pop(0)
+    titre_url = titre.replace(" ","_")
+    config.configs['helpers.auto_fields.AutoLang'].auto_lang = "en"
+    base_url = "http://127.0.0.1:5500"
+    manifest = Manifest(id=f"{base_url}/{titre_url}.json", label=titre)
 
 
-        for i in link:
+    #Creation des Canvas 
+
+    Recuperation.pop(0)
+
+
+    for i in link:
 
             if format[num] == "video"  :
-                canvas = manifest.make_canvas(id=f"{base_url}/canvas/{num}/")
+
+                canvas = manifest.make_canvas(id=f"{base_url}/canvas/{num}")
                 anno_body = ResourceItem(id=link[int(f"{num-1}")],
                                 type="Video",
                                 format="video/MPG")
 
-                anno_page = AnnotationPage(id=f"{base_url}/canvas/page")
-                anno = Annotation(id=f"{base_url}/canvas/page/annotation",
+                anno_page = AnnotationPage(id=f"{base_url}/canvas/{num}/page/1")
+                anno = Annotation(id=f"{base_url}/canvas/{num}/page/1/1",
                             motivation="painting",
                             body=anno_body,
                             target=canvas.id)
@@ -70,6 +67,7 @@ def md_to_manifest(markdown_folder, manifest_folder, mail, password) :
             if format[num] == "image"  :
 
                 img =Image.open(urlopen(i))
+
                 widh_media, height_media = img.size
 
                 if i.find("PNG") > 0 or i.find("png") > 0 :
@@ -80,69 +78,67 @@ def md_to_manifest(markdown_folder, manifest_folder, mail, password) :
 
                 canvas = manifest.make_canvas(id=f"{base_url}/canvas{num}/p1", height=height_media, width=widh_media)
                 anno_page = canvas.add_image(image_url = i,
-                                                anno_page_id=f"{base_url}/page/p1/1",
-                                                anno_id=f"{base_url}/annotation/p0001-image",
+                                                anno_page_id=f"{base_url}/canvas/{num}/page/1",
+                                                anno_id=f"{base_url}/canvas/{num}/page/1/1",
                                                 format=f"image/{format_image}",
                                                 height=widh_media,
                                                 width=height_media
                                                 )
 
-            num = num + 1
+    num = num + 1
 
 
-        #Export manifest en .json 
+    #Export manifest en .json 
 
-        nom_manifest = f"manifest_essai{num_fichier}.json"
-        manifest_path = os.path.join(manifest_folder, nom_manifest)
-        with open(manifest_path, 'w', encoding='utf-8') as f:
+    nom_manifest = f"{titre_url}.json"
+    manifest_path = os.path.join(manifest_folder, nom_manifest)
+
+    with open(manifest_path, 'w', encoding='utf-8') as f:
             json.dump(manifest.dict(), f, ensure_ascii=False, indent=2)
 
-                #Upload Arvest
+    #Upload Arvest
 
-        new_manifest = ar.add_manifest(path = manifest_path, update_id = True)
-        new_manifest.update_title(titre)
-        print(new_manifest.title)
+    new_manifest = ar.add_manifest(path = manifest_path, update_id = True)
+    new_manifest.update_title(titre)
+    print(new_manifest.title)
 
-        num_fichier = num_fichier +1 
-
-        num = 0
+    num = 0
 
 
 
-        
+
+
 def metadata_update(markdown_folder, manifest_folder, mail, password):
 
-  ar = arvestapi.Arvest(mail, password)
+    ar = arvestapi.Arvest(mail, password)
+    print("Update metadata...")
 
-  print("Update metadata...")
+    #Ouverture du markdown
 
-  #Ouverture du markdown
 
-  num_fichier = 25
-  nbr = os.listdir(markdown_folder)
-  md_count = 0
-  for i in nbr :
-      md_count += 1
+    chemin = os.path.join(markdown_folder)
 
-  for i in range(1): 
-
-    chemin = os.path.join(markdown_folder, f"Manifest_{num_fichier}.md")
-    
     Contenu_canvas = recuperation(['## Manifest metadata'],['@@@m'], chemin)
+
     Base_info = recuperation(['## Manifest title '],['@@@t'], chemin)
+
     Labels = extraction_colonne("Label", Contenu_canvas)
     Value = extraction_colonne("Value", Contenu_canvas)
+
     titre = extraction_metadonne("Title", Base_info)
     thumb = extraction_metadonne("thumbnail", Base_info)
 
     #Recuperation du manifest
 
     manifestes = ar.get_manifests()
+
     for item in manifestes:
       if item.title == titre:
         contenu = item.get_content()
         id = item.id
+
     manifeste = ar.get_manifest(id)
+
 
     url = contenu["items"][0]["items"][0]["items"][0]["body"]["id"]
 
@@ -156,6 +152,7 @@ def metadata_update(markdown_folder, manifest_folder, mail, password):
             dicto = {"label":f"{i}","value":f"{Value[numo]}"}
             metadonnee.append(dicto)
             numo += 1
+
     contenu["metadata"] = metadonnee
 
 
@@ -163,6 +160,7 @@ def metadata_update(markdown_folder, manifest_folder, mail, password):
 
     img = Image.open(urlopen(thumb))
     widh_media, height_media = img.size
+
     thumbnail = [
       {
         "id": f"{thumb}",
@@ -172,113 +170,115 @@ def metadata_update(markdown_folder, manifest_folder, mail, password):
         "height": height_media
       }
       ]
+
     contenu["thumbnail"] = thumbnail
 
-
-    #Export du manifest en .json
-
-    nom_manifest = f"{titre}.json"
 
     #Update du manifeste
 
     manifeste.update_content(contenu)
     print("Metadata successfully updated !")
 
-    num_fichier = num_fichier +1
-
+    
+  
 
 
 def md_to_anotation(markdown_folder, manifest_folder, mail, password) :
 
+
       ar = arvestapi.Arvest(mail, password)
 
-      num_fichier = 25
-      nbr = os.listdir(markdown_folder)
-      md_count = 0
-      for i in nbr :
-            md_count += 1
-      md_count -= 1
 
-      for i in range(1): 
 
-            chemin = os.path.join(markdown_folder, f"Manifest_{num_fichier}.md")
+      chemin = (markdown_folder)
 
-            #Extraction des informations pour les Canvas------------------------------------------
+      #Extraction des informations pour les Canvas------------------------------------------
 
-            Recuperation_titres = recuperation(['## Manifest metadata'],['@@@m'], chemin)
-            titre = extraction_metadonne("Title", Recuperation_titres)
-            manifestes = ar.get_manifests()
+      Recuperation_titres = recuperation(['## Manifest metadata'],['@@@m'], chemin)
+      titre = extraction_metadonne("Title", Recuperation_titres)
 
-            for item in manifestes:
+      manifestes = ar.get_manifests()
+
+      for item in manifestes:
                   if item.title == titre:
                         contenu = item.get_content()
                         #manifeste = item
                         ident = item.id
 
-            manifeste = ar.get_manifest(ident)
-            body = contenu["items"]
+      manifeste = ar.get_manifest(ident)
 
-            #Extraction des informations pour les Canvas------------------------------------------
+      body = contenu["items"]
 
-            Recuperation = recuperation(['## Text Annotations'],['@@@ta'], chemin)
-            annot = extraction_colonne("Contenu", Recuperation)
-            durations = extraction_duration(Recuperation, 2, 3)
-            canva = extraction_colonne("Canvas", Recuperation)
-            Recuperation_link = recuperation(['## Link Annotations'],['@@@la'], chemin)
-            link = extraction_colonne("Manifest", Recuperation_link)
-            content = extraction_colonne("Contenu", Recuperation_link)
+      #Extraction des informations pour les Canvas------------------------------------------
 
-            #Transformation des url en lien cliquable 
+      Recuperation = recuperation(['## Text Annotations'],['@@@ta'], chemin)
+      annot = extraction_colonne("Contenu", Recuperation)
+      durations = extraction_duration(Recuperation, 2, 3)
+      canva = extraction_colonne("Canvas", Recuperation)
 
-            http = "https:"
-            link_conversion(http, annot)
-            http = "http:"
-            link_conversion(http, annot)
+      Recuperation_link = recuperation(['## Link Annotations'],['@@@la'], chemin)
+      link = extraction_colonne("Manifest", Recuperation_link)
+      content = extraction_colonne("Contenu", Recuperation_link)
+
+      Base_info = recuperation(['## Manifest title '],['@@@t'], chemin)
+      titre = extraction_metadonne("Title", Base_info)
+
+      #Transformation des url en lien cliquable 
+
+      http = "https:"
+      link_conversion(http, annot)
+      http = "http:"
+      link_conversion(http, annot)
 
 
-            #Creation d'une base d'annotation------------------------------------------
+      #Creation d'une base d'annotation------------------------------------------
 
-            base_url = manifeste.get_full_url()
-            base_url = base_url.replace(".json","")
+      base_url = manifeste.get_full_url()
+      base_url = base_url.replace(".json","")
 
-            canvas_id = contenu["items"][0]["id"]
-            manifest = Manifest(id=f"{base_url}/manifest_essai2.json",
-                    label={"en": ["Scott"]})
+      canvas_id = contenu["items"][0]["id"]
 
-            num = 0
+      titre_url = titre.replace(" ","_")
 
-            # Detecteur d'annotation lien------------------------------------------
+      manifest = Manifest(id=f"{base_url}",
+                    label=titre)
 
-            def verifi(annotation):
+      num = 0
+
+      # Detecteur d'annotation lien------------------------------------------
+
+      def verifi(annotation):
                   resultat = False
                   for i in content :
                         if i == annotation :
                               resultat = True 
                   return resultat 
-            num_par_canvas = []
 
-            # Supression des doublons dans la liste des Canvas---------------------
+      num_par_canvas = []
 
-            def anti_doublon(dicto, liste):
+      # Supression des doublons dans la liste des Canvas---------------------
+
+      def anti_doublon(dicto, liste):
                   resultat = False
                   for i in liste :
                         if i == dicto :
                               resultat = True 
                   return resultat 
 
-            for i in canva :
+      for i in canva :
                   dicto = {"canvas":f"{i}","value": 0}
                   if anti_doublon(dicto, num_par_canvas) is False :
                     num_par_canvas.append(dicto)
 
-            # Pour chaque annotation dans le markdown
+      # Pour chaque annotation dans le markdown
 
-            for i in annot : 
-                
+      for i in annot : 
+                  
                   num = num +1
 
                   #Recuperation du numero et id du canvas
                   numero = canva[num-1]
+
                   canvas_id = (body[int(numero)-1]["id"])
 
                   # Compte si y a deja eu une annotation dans le canvas
@@ -322,6 +322,7 @@ def md_to_anotation(markdown_folder, manifest_folder, mail, password) :
                                           anno_page_id=f"{base_url}/page/p1/1")
 
                   rag = canvas.json(indent=2)
+
                   raga = json.loads(rag)
 
 
@@ -330,8 +331,9 @@ def md_to_anotation(markdown_folder, manifest_folder, mail, password) :
                   if num_par_canvas[int(numero)-1]["value"] > 1 : 
                         contenu["items"][int(numero)-1]["annotations"][0]["items"] = raga["annotations"][0]["items"]
                   
-            #Upload sur Arvest
+      #Upload sur Arvest
 
-            manifeste.update_content(contenu)
-            print(f"{titre} was successfuly annotated !" )
+      manifeste.update_content(contenu)
+      print(f"{titre} was successfuly annotated !" )
+      print("-------")
             
